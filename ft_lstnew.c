9511cfb7d9119413d64aabb9ad0de7d86a993396
Lauri Litovuo <llitovuo@student.hive.fi>
2023-12-27 15:42:17 +0200

    adding lstnew bonus exercise

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 12:13:01 by llitovuo          #+#    #+#             */
+/*   Updated: 2023/11/15 15:48:50 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *) malloc(sizeof(new));
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
